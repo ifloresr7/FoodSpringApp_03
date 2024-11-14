b7e72366da8b35defd65254cc320afe3f10009a0
cbravose <cbravose@uoc.edu>
2024-11-14 08:27:27 +0100

    .security evitar csrf

--- src/main/java/com/FoodSpringApp/FoodSpringApp/config/SecurityConfig.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/config/SecurityConfig.java
@@ -1,27 +1,37 @@
 package com.FoodSpringApp.FoodSpringApp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-           
-            .formLogin(formLogin -> 
-                formLogin
-                    .loginPage("/login")            
-                    .permitAll()
-            )
-            .logout(logout -> 
-                logout
-                    .permitAll()
-            );
+       
 
-        return http.build();
+        http
+         .csrf(csrf -> csrf
+                .ignoringRequestMatchers("/api/**") // Desactiva CSRF solo para las rutas que comienzan con "/api/"
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // Personaliza el repositorio de tokens CSRF usando cookies
+            )
+        .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/", "/vehiculos", "/login","/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+            .requestMatchers("/alquileres").authenticated()
+            .requestMatchers("/admin","/clientes").hasRole("ADMIN")
+        )
+        .formLogin((form) -> form
+            .loginPage("/login")
+            .permitAll()
+            .failureUrl("/login?error=true")
+        )
+        .logout((logout) -> logout.permitAll());
+    return http.build();
+
     }
 }
